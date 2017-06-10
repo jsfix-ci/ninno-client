20937208c73339b9b849e927b7f55275e591ebe4
Ivar Nilsen <post@ivarnilsen.com>
2017-06-10 13:25:18 +0200

    Show age, closes #7

--- src/components/date-picker/DateDisplay.js
+++ src/components/date-picker/DateDisplay.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { instanceOf } from 'prop-types';
 
+import differenceInYears from 'date-fns/difference_in_years';
 import format from 'date-fns/format';
 
 function DateDisplay(props) {
     const { date } = props;
 
+    const age = differenceInYears(new Date(), date);
+
     return (
-        <div className="ninno-date-picker__component">
-            <span className="ninno-date-picker__date-display">
-                Valgt dato: <strong>{format(date, 'DD.MM.YYYY')}</strong>
-            </span>
+        <div>
+            <div className="ninno-date-picker__component">
+                <span className="ninno-date-picker__date-display">
+                    Valgt dato: <strong>{format(date, 'DD.MM.YYYY')}</strong>
+                </span>
+            </div>
+            <div className="ninno-date-picker__component">
+                <span className="ninno-date-picker__date-display">
+                    Valgt alder: <strong>{age}</strong>
+                </span>
+            </div>
         </div>
     );
 }
